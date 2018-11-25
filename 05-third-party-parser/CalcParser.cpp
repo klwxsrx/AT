9c3c7093452d5d49afe08944ccb99791e0973a73
Viatcheslav Bartsev <Slavyan58@yandex.ru>
2018-11-25 16:06:51 +0300

    Поправил все проблемы компиляции генерируемого парсера

--- 05-third-party-parser/CalcParser.cpp
+++ 05-third-party-parser/CalcParser.cpp
@@ -5,9 +5,29 @@ double CalcParser::Calculate(std::string const& source)
     return 1.0;
 }
 
+double CalcParser::GetIdValue(const char* id) const
+{
+    auto it = m_variables.find(id);
+    if (it == m_variables.end())
+    {
+        throw std::runtime_error("Variable not found");
+    }
+    return it->second;
+}
+
+void CalcParser::SetIdValue(const char* id, double value)
+{
+    m_variables[id] = value;
+}
+
+void CalcParser::SetResult(double result)
+{
+    m_result = result;
+}
+
 void CalcParser::OnError(LemonToken const& token)
 {
-    throw std::runtime_error("Error!");
+    throw std::runtime_error("Error while parse expression!");
 }
 
 void CalcParser::OnStackOverflow()
--- 05-third-party-parser/CalcParser.h
+++ 05-third-party-parser/CalcParser.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #include <stdexcept>
+#include <map>
 #include "ICalcParser.h"
 #include "LemonToken.h"
 
@@ -7,6 +9,13 @@ class CalcParser final : public ICalcParser {
 public:
     double Calculate(std::string const& source) override;
 
+    double GetIdValue(const char *id) const;
+    void SetIdValue(const char *id, double value);
+    void SetResult(double result);
     void OnError(LemonToken const& token);
     void OnStackOverflow();
+
+private:
+    std::map<std::string, double> m_variables;
+    double m_result = 0;
 };
